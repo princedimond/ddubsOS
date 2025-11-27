f3e62b76a314eaa9238ea6d235a507b75b0b1bf9
princedimond <princedimond@gmail.com>
2025-11-26 23:33:45 -0600

    updated all packages
    warp terminal is still broken

--- modules/home/hyprland/misc.nix
+++ modules/home/hyprland/misc.nix
@@ -1,9 +1,10 @@
-{ ... }: {
+{ ... }:
+{
   wayland.windowManager.hyprland = {
     settings = {
       misc = {
         focus_on_activate = false;
-        disable_hyprland_qtutils_check = true;
+        #disable_hyprland_qtutils_check = true;
         mouse_move_focuses_monitor = true;
         layers_hog_keyboard_focus = true;
         initial_workspace_tracking = 0;
